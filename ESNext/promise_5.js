e7aec3b5514acf0bb0359b192794e763a735824d
Douglas Bordinassi <bordinassidouglas@gmail.com>
2022-03-22 10:14:56 -0300

    Continuação dos estudos

---
+++ ESNext/promise_5.js
@@ -0,0 +1,15 @@
+function funcionarOuNao(valor,chanceErro){
+    return new Promise((resolve, reject) => {
+        if(Math.random() < chanceErro){
+            reject('Ocorreu um erro!')
+        }else{
+            resolve(valor)
+        }
+    })
+}
+
+funcionarOuNao('Testando...', 0.5)
+    .then(v => `Valor: ${v}`)
+    .then(v => console.log(v))
+    .catch(erro => console.log(`Erro: ${erro}`))
+    .then(() => console.log('Fim!'))
